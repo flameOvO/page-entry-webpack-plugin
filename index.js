2af8a7e68e51739ede946d8dfbd64df9e70b7ddc
xingming <xingming@webuy.ai>
2021-01-05 14:56:39 +0800

    feat: 支持html-webpack-plugin v3

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const debug = require('debug')('page-entry-webpack-plugin');
 const htmlAssetsList = new Set();
 // 一个 JavaScript 命名函数。
-class MultiEntryWebpackPlugin {
+class pageEntryWebpackPlugin {
 
   constructor(options) {
-    debug('MultiEntryWebpackPlugin--------start');
+    debug('pageEntryWebpackPlugin--------start');
     const defaultOptions = {
       filename: 'index.html',
       template: 'auto',
@@ -27,27 +27,19 @@ class MultiEntryWebpackPlugin {
     if (path.resolve(filename) === path.normalize(filename)) {
       this.options.filename = path.relative(compiler.options.output.path, filename);
     }
-
-    compiler.hooks.emit.tapAsync('MultiEntryWebpackPlugin', function(compilation, callback) {
-      debug('MultiEntryWebpackPlugin--------emit');
-      const hooks = HtmlWebpackPlugin.getHooks(compilation);
-      hooks.afterEmit.tapAsync('MultiEntryWebpackPlugin', (htmlPluginData, cb) => {
-        debug('htmlPluginData', htmlPluginData.outputName);
-        htmlAssetsList.add(htmlPluginData.outputName);
-        const template = fs.readFileSync(self.options.template, 'utf-8');
-        const html = ejs.render(template, {assets: Array.from(htmlAssetsList), pluginOptions: self.options});
-      
-        // 将这个列表作为一个新的文件资源，插入到 webpack 构建中：
-        compilation.assets[self.options.filename] = {
-          source: function() {
-            return html;
-          },
-          size: function() {
-            return html.length;
-          }
-        };
-        cb();
-      });
+    
+    compiler.hooks.emit.tapAsync('pageEntryWebpackPlugin', function(compilation, callback) {
+      debug('pageEntryWebpackPlugin--------emit');
+      if (compilation.hooks.htmlWebpackPluginBeforeHtmlGeneration) {
+        compilation.hooks.htmlWebpackPluginAfterEmit.tapAsync('pageEntryWebpackPlugin', (htmlPluginData, cb) => {
+          self.renderTemplate(compilation, htmlPluginData, cb);
+        });
+      } else {
+        const hooks = HtmlWebpackPlugin.getHooks(compilation);
+        hooks.afterEmit.tapAsync('pageEntryWebpackPlugin', (htmlPluginData, cb) => {
+          self.renderTemplate(compilation, htmlPluginData, cb);
+        });
+      }
       callback();
     });
 
@@ -72,5 +64,23 @@ class MultiEntryWebpackPlugin {
       /([!])([^/\\][^!?]+|[^/\\!?])($|\?[^!?\n]+$)/,
       (match, prefix, filepath, postfix) => prefix + path.resolve(filepath) + postfix);
   }
+
+  renderTemplate(compilation, htmlPluginData, callback) {
+    debug('htmlPluginData', htmlPluginData.outputName);
+    htmlAssetsList.add(htmlPluginData.outputName);
+    const template = fs.readFileSync(this.options.template, 'utf-8');
+    const html = ejs.render(template, {assets: Array.from(htmlAssetsList), pluginOptions: this.options});
+  
+    // 将这个列表作为一个新的文件资源，插入到 webpack 构建中：
+    compilation.assets[this.options.filename] = {
+      source: function() {
+        return html;
+      },
+      size: function() {
+        return html.length;
+      }
+    };
+    callback();
+  }
 }
-module.exports = MultiEntryWebpackPlugin;
+module.exports = pageEntryWebpackPlugin;
